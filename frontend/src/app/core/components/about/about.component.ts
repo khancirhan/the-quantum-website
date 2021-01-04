4379dec8e411c3dfbb3fe9e481cabd6422c67649
Cirhan Yaqoob <khancirhan@gmail.com>
2021-01-04 16:00:39 +0530

    About Page video gallery

--- frontend/src/app/core/components/about/about.component.ts
+++ frontend/src/app/core/components/about/about.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { LightboxService } from 'src/app/shared/lightbox/service/lightbox.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent implements OnInit {
+  constructor(private renderer: Renderer2, private lightbox: LightboxService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  showPlayBtn(el: HTMLElement): void {
+    this.renderer.addClass(el, 'visible');
   }
 
+  hidePlayBtn(el: HTMLElement): void {
+    this.renderer.removeClass(el, 'visible');
+  }
+
+  openInLightbox(url: string): void {
+    this.lightbox.open(url);
+  }
 }
